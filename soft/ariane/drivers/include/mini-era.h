ee7bc34450def81eb422e8dcee754dd08a921eef
J-D Wellman <jdwellman@yahoo.com>
2020-01-06 10:09:34 -0500

    Support includes for mini-era application(s) running on an ESP SoC (on FPGA) system with some hardware accelerators.

---
+++ soft/ariane/drivers/include/mini-era.h
@@ -0,0 +1,46 @@
+#ifndef _MINI_ERA_H_
+#define _MINI_ERA_H_
+
+#ifdef __KERNEL__
+#include <linux/ioctl.h>
+#include <linux/types.h>
+#else
+#include <sys/ioctl.h>
+#include <stdint.h>
+#ifndef __user
+#define __user
+#endif
+#endif /* __KERNEL__ */
+
+#include <esp.h>
+#include <esp_accelerator.h>
+
+struct vitbfly2_access {
+	struct esp_access esp;
+};
+
+#define VITBFLY2_IOC_ACCESS	_IOW ('S', 0, struct vitbfly2_access)
+
+
+
+/* <<--params-def-->> */
+#define FFTHW_LEN      16384
+#define FFTHW_LOG_LEN     14
+
+/* <<--params-->> */
+//const int32_t fftHW_len = FFTHW_LEN;
+//const int32_t fftHW_log_len = FFTHW_LOG_LEN;
+
+struct fftHW_access {
+	struct esp_access esp;
+        /* <<--regs-->> */
+        unsigned len;
+        unsigned log_len;
+        unsigned src_offset;
+        unsigned dst_offset;
+};
+
+#define FFTHW_IOC_ACCESS	_IOW ('S', 0, struct fftHW_access)
+
+
+#endif /* _MINI_ERA_H_ */
