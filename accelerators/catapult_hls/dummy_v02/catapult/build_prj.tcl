0d9096286543bc75cdbcabcb6008a0d7107beee7
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2020-04-01 18:33:28 -0400

    Add a little less dummy design which does some computation (output[i]=input[i]+1)

---
+++ accelerators/catapult_hls/dummy_v02/catapult/build_prj.tcl
@@ -0,0 +1,309 @@
+if {$opt(asic) > 0} {
+    project new -name Catapult_asic
+    set CSIM_RESULTS "./tb_data/catapult_asic_csim_results.log"
+    set RTL_COSIM_RESULTS "./tb_data/catapult_asic_rtl_cosim_results.log"
+} else {
+    project new -name Catapult_fpga
+    set CSIM_RESULTS "./tb_data/catapult_fpga_csim_results.log"
+    set RTL_COSIM_RESULTS "./tb_data/catapult_fpga_rtl_cosim_results.log"
+}
+
+#
+# Reset the options to the factory defaults
+#
+
+solution new -state initial
+solution options defaults
+
+solution options set Flows/ModelSim/VLOG_OPTS {-suppress 12110}
+solution options set Flows/ModelSim/VSIM_OPTS {-t ps -suppress 12110}
+solution options set Flows/DesignCompiler/OutNetlistFormat verilog
+solution options set /Input/CppStandard c++11
+#solution options set /Input/TargetPlatform x86_64
+
+solution options set Cache/UserCacheHome "catapult_105c_cache"
+#solution options set Cache/UserCacheHome "catapul_105beta_cache"
+solution options set Cache/DefaultCacheHomeEnabled false
+
+flow package require /SCVerify
+#options set Flows/OSCI/GCOV true
+#flow package require /CCOV
+#flow package require /SLEC
+#flow package require /CDesignChecker
+
+#directive set -DESIGN_GOAL area
+##directive set -OLD_SCHED false
+#directive set -SPECULATE true
+#directive set -MERGEABLE true
+directive set -REGISTER_THRESHOLD 8192
+#directive set -MEM_MAP_THRESHOLD 32
+#directive set -LOGIC_OPT false
+#directive set -FSM_ENCODING none
+#directive set -FSM_BINARY_ENCODING_THRESHOLD 64
+#directive set -REG_MAX_FANOUT 0
+#directive set -NO_X_ASSIGNMENTS true
+#directive set -SAFE_FSM false
+#directive set -REGISTER_SHARING_MAX_WIDTH_DIFFERENCE 8
+#directive set -REGISTER_SHARING_LIMIT 0
+#directive set -ASSIGN_OVERHEAD 0
+#directive set -TIMING_CHECKS true
+#directive set -MUXPATH true
+#directive set -REALLOC true
+#directive set -UNROLL no
+#directive set -IO_MODE super
+#directive set -CHAN_IO_PROTOCOL standard
+#directive set -ARRAY_SIZE 1024
+#directive set -REGISTER_IDLE_SIGNAL false
+#directive set -IDLE_SIGNAL {}
+#directive set -STALL_FLAG false
+directive set -TRANSACTION_DONE_SIGNAL true
+#directive set -DONE_FLAG {}
+#directive set -READY_FLAG {}
+#directive set -START_FLAG {}
+#directive set -BLOCK_SYNC none
+#directive set -TRANSACTION_SYNC ready
+#directive set -DATA_SYNC none
+#directive set -CLOCKS {clk {-CLOCK_PERIOD 0.0 -CLOCK_EDGE rising -CLOCK_UNCERTAINTY 0.0 -RESET_SYNC_NAME rst -RESET_ASYNC_NAME arst_n -RESET_KIND sync -RESET_SYNC_ACTIVE high -RESET_ASYNC_ACTIVE low -ENABLE_ACTIVE high}}
+#directive set -RESET_CLEARS_ALL_REGS true
+#directive set -CLOCK_OVERHEAD 20.000000
+#directive set -OPT_CONST_MULTS use_library
+#directive set -CHARACTERIZE_ROM false
+#directive set -PROTOTYPE_ROM true
+#directive set -ROM_THRESHOLD 64
+#directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
+#directive set -CLUSTER_OPT_CONSTANT_INPUTS true
+#directive set -CLUSTER_RTL_SYN false
+#directive set -CLUSTER_FAST_MODE false
+#directive set -CLUSTER_TYPE combinational
+#directive set -COMPGRADE fast
+
+#set CLOCK_PERIOD 12.5
+
+# Design specific options.
+
+#
+# Flags
+#
+
+if {$opt(asic) > 0} {
+solution options set Flows/QuestaSIM/SCCOM_OPTS {-g -x c++ -Wall -Wno-unused-label -Wno-unknown-pragmas -DDMA_WIDTH=64 -DCLOCK_PERIOD=12500}
+} else {
+solution options set Flows/QuestaSIM/SCCOM_OPTS {-g -x c++ -Wall -Wno-unused-label -Wno-unknown-pragmas -DDMA_WIDTH=64 -DCLOCK_PERIOD=12500}
+}
+
+if {$opt(channels) == 0} {
+solution options set /Input/CompilerFlags {-DDMA_WIDTH=64 -DCLOCK_PERIOD=12500}
+} else {
+solution options set /Input/CompilerFlags {-DDMA_WIDTH=64 -DHLS_CATAPULT -D__MNTR_CONNECTIONS__ -DCLOCK_PERIOD=12500}
+}
+
+#
+# Output
+#
+
+solution options set /Input/SearchPath { \
+    ../src \
+    ../tb \
+    ../../common/syn-templates \
+    ../../common/syn-templates/core \
+    ../../common/syn-templates/core/accelerators \
+    ../../common/syn-templates/core/systems \
+    ../../common/syn-templates/utils \
+    ../../common/syn-templates/utils/configs }
+
+# Add source files.
+solution file add ../src/dummy.cpp -type C++
+solution file add ../tb/sc_main.cpp -type C++ -exclude true
+solution file add ../tb/system.cpp -type C++ -exclude true
+
+#solution file set ../tb/sc_main.cpp -args {-DDISABLE_PRINTF}
+
+
+#
+# Output
+#
+
+# Verilog only
+solution option set Output/OutputVHDL false
+solution option set Output/OutputVerilog true
+
+# Package output in Solution dir
+solution option set Output/PackageOutput true
+solution option set Output/PackageStaticFiles true
+
+# Add Prefix to library and generated sub-blocks
+solution option set Output/PrefixStaticFiles true
+solution options set Output/SubBlockNamePrefix "esp_acc_dummy_"
+
+# Do not modify names
+solution option set Output/DoNotModifyNames true
+
+go new
+
+#
+#
+#
+
+go analyze
+
+#
+#
+#
+
+
+# Set the top module and inline all of the other functions.
+
+# 10.4c
+#directive set -DESIGN_HIERARCHY dummy
+
+# 10.5
+solution design set dummy -top
+
+#directive set PRESERVE_STRUCTS false
+
+#
+#
+#
+
+go compile
+
+# Run C simulation.
+if {$opt(csim)} {
+    flow run /SCVerify/launch_make ./scverify/Verify_orig_cxx_osci.mk {} SIMTOOL=osci sim
+}
+
+#
+#
+#
+
+# Run HLS.
+if {$opt(hsynth)} {
+
+    if {$opt(asic) == 1} {
+        solution library add nangate-45nm_beh -- -rtlsyntool DesignCompiler -vendor Nangate -technology 045nm
+        solution library add ccs_sample_mem
+    } elseif {$opt(asic) == 2} {
+        solution library add nangate-45nm_beh -- -rtlsyntool RTLCompiler -vendor Nangate -technology 045nm
+        solution library add ccs_sample_mem
+    } elseif {$opt(asic) == 3} {
+        puts "ERROR: Cadence Genus is not supported"
+        exit 1
+    } else {
+
+        solution library add mgc_Xilinx-VIRTEX-uplus-2_beh -- -rtlsyntool Vivado -manufacturer Xilinx -family VIRTEX-uplus -speed -2 -part xcvu9p-flga2104-2-e
+        #solution library add mgc_Xilinx-VIRTEX-u-2_beh -- -rtlsyntool Vivado -manufacturer Xilinx -family VIRTEX-u -speed -2 -part xcvu440-flga2892-2-e
+        #solution library add mgc_Xilinx-VIRTEX-7-2_beh -- -rtlsyntool Vivado -manufacturer Xilinx -family VIRTEX-7 -speed -2 -part xc7v2000tflg1925-2
+
+
+        solution library add Xilinx_RAMS
+        solution library add Xilinx_ROMS
+        solution library add Xilinx_FIFO
+    }
+
+    go libraries
+
+    #
+    #
+    #
+
+    directive set -CLOCKS { \
+        clk { \
+            -CLOCK_PERIOD 12.5 \
+            -CLOCK_HIGH_TIME 6.25 \
+            -CLOCK_OFFSET 0.000000 \
+            -CLOCK_UNCERTAINTY 0.0 \
+        } \
+    }
+
+    # BUGFIX: This prevents the creation of the empty module CGHpart. In the
+    # next releases of Catapult HLS, this may be fixed.
+    directive set /dummy -GATE_EFFORT normal
+
+    go assembly
+
+    #
+    #
+    #
+
+    # Top-Module I/O
+
+    # Arrays
+
+    directive set /dummy/dummy:load_input/load_input/LOAD_INPUT_TOKENS_LOOP:plm_local.data:rsc -MAP_TO_MODULE Xilinx_RAMS.BLOCK_1R1W_RBW
+
+#    directive set /dummy/dummy:load_input/plm0_in:cns -MAP_TO_MODULE ccs_ioport.ccs_out_wait
+#    directive set /dummy/dummy:load_input/plm0_in:cns -PACKING_MODE sidebyside
+#    #directive set /dummy/dummy:load_input/plm0 -WORD_WIDTH 32768
+#
+#    directive set /dummy/dummy:load_input/plm1:cns -MAP_TO_MODULE mgc_ioport.mgc_out_stdreg_wait
+#    directive set /dummy/dummy:load_input/plm1:cns -PACKING_MODE sidebyside
+#    #directive set /dummy/dummy:load_input/plm1 -WORD_WIDTH 32768
+#
+#    directive set /dummy/dummy:store_output/plm0:cns -MAP_TO_MODULE ccs_ioport.ccs_in_wait
+#    directive set /dummy/dummy:store_output/plm0:cns -PACKING_MODE sidebyside
+#    #directive set /dummy/dummy:store_output/plm0 -WORD_WIDTH 32768
+#
+#    directive set /dummy/dummy:store_output/plm1:cns -MAP_TO_MODULE mgc_ioport.mgc_chan_in
+#    directive set /dummy/dummy:store_output/plm1:cns -PACKING_MODE sidebyside
+#    #directive set /dummy/dummy:store_output/plm1 -WORD_WIDTH 32768
+#
+#    directive set /dummy/plm0:cns -MAP_TO_MODULE ccs_ioport.ccs_pipe
+#    directive set /dummy/plm0:cns -PACKING_MODE sidebyside
+#    #directive set /dummy/plm0 -WORD_WIDTH 32768
+#
+#    directive set /dummy/plm1:cns -MAP_TO_MODULE mgc_ioport.mgc_pipe
+#    directive set /dummy/plm1:cns -PACKING_MODE sidebyside
+#    #directive set /dummy/plm1 -WORD_WIDTH 32768
+
+    # Loops
+
+    directive set /dummy/dummy:load_input/load_input/LOAD_INPUT_DATA_LOOP -PIPELINE_INIT_INTERVAL 1
+    directive set /dummy/dummy:store_output/store_output/STORE_OUTPUT_DATA_LOOP -PIPELINE_INIT_INTERVAL 1
+
+    # Area vs Latency Goals
+
+    directive set /dummy/config_accelerator -DESIGN_GOAL latency
+    directive set /dummy/dummy:load_input/load_input -DESIGN_GOAL latency
+    directive set /dummy/dummy:compute_kernel/compute_kernel -DESIGN_GOAL latency
+    directive set /dummy/dummy:store_output/store_output -DESIGN_GOAL latency
+
+#    go architect
+#
+#    #
+#    #
+#    #
+#
+#    go allocate
+#
+#    #
+#    # RTL
+#    #
+#
+#    go extract
+#
+#    #
+#    #
+#    #
+#
+#    if {$opt(rtlsim)} {
+#        #flow run /SCVerify/launch_make ./scverify/Verify_concat_sim_dummy_v_msim.mk {} SIMTOOL=msim sim
+#        flow run /SCVerify/launch_make ./scverify/Verify_concat_sim_dummy_v_msim.mk {} SIMTOOL=msim simgui
+#    }
+#
+#    if {$opt(lsynth)} {
+#
+#        if {$opt(asic) == 1} {
+#            flow run /DesignCompiler/dc_shell ./concat_rtl.v.dc v
+#        } elseif {$opt(asic) == 2} {
+#            flow run /RTLCompiler/rc ./concat_rtl.v.rc v
+#        } elseif {$opt(asic) == 3} {
+#            puts "ERROR: Cadence Genus is not supported"
+#            exit 1
+#        } else {
+#            flow run /Vivado/synthesize -shell vivado_concat_v/concat_rtl.v.xv
+#        }
+#
+#    }
+}
+
+project save
---
+++ accelerators/catapult_hls/dummy_v02/src/dummy.cpp
@@ -0,0 +1,508 @@
+// Copyright (c) 2011-2019 Columbia University, System Level Design Group
+// SPDX-License-Identifier: Apache-2.0
+
+#include "dummy.hpp"
+#include "dummy_directives.hpp"
+
+// Functions
+
+#include "dummy_functions.hpp"
+
+// Processes
+
+void dummy::load_input()
+{
+
+    // Reset
+    {
+        HLS_PROTO("load-reset");
+        this->reset_load_input();
+        wait();
+    }
+
+    // Config
+    uint32_t tokens;
+    uint32_t batch;
+    {
+        HLS_PROTO("load-config");
+
+        wait_for_config(); // config process
+        conf_info_t config = this->conf_info.read();
+
+        tokens = config.tokens;
+        batch = config.batch;
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load config(): tokens = %llu, batch = %llu", ESP_TO_UINT64(tokens), ESP_TO_UINT64(batch));
+    }
+
+    // Load
+    bool ping = true;
+    uint32_t offset = 0;
+
+LOAD_INPUT_BATCH_LOOP:
+    for (unsigned n = 0; n < batch; n++)
+    {
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): batch # %d start...", n+1);
+
+LOAD_INPUT_TOKENS_LOOP:
+        for (int b = tokens; b > 0; b -= PLM_SIZE)
+        {
+            HLS_PROTO("load-dma");
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): ping = %d", ping);
+
+            uint32_t len = b > PLM_SIZE ? PLM_SIZE : b;
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): len = %llu [max %d]", ESP_TO_UINT64(len), PLM_SIZE);
+
+            dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
+            offset += len;
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): dma_info.index = %llu, dma_info.length = %llu, dma_info.size = %llu", ESP_TO_UINT64(dma_info.index), ESP_TO_UINT64(dma_info.length), dma_info.size.to_uint64());
+
+#ifdef DMA_SINGLE_PROCESS
+#if (__MNTR_CONNECTIONS__)
+            this->dma_read_ctrl.PushNB(dma_info);
+#else
+            this->dma_read_ctrl.nb_write(dma_info);
+#endif
+#else
+#if (__MNTR_CONNECTIONS__)
+            this->dma_read_ctrl.Push(dma_info);
+#else
+            this->dma_read_ctrl.write(dma_info);
+#endif
+#endif
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): dma_read_ctrl done!");
+
+            plm_t plm_local;
+LOAD_INPUT_DATA_LOOP:
+            for (uint16_t i = 0; i < len; i++) {
+                uint64_t data;
+                sc_dt::sc_bv<64> data_bv;
+
+#if (DMA_WIDTH == 64)
+                ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): dma_read_chnl...");
+
+#if (__MNTR_CONNECTIONS__)
+                data_bv = this->dma_read_chnl.Pop();
+#else
+                data_bv = this->dma_read_chnl.read();
+#endif
+
+                ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): dma_read_chnl done! (%llX)", data_bv.to_uint64());
+
+#elif (DMA_WIDTH == 32)
+
+#if (__MNTR_CONNECTIONS__)
+                data_bv.range(31, 0) = this->dma_read_chnl.Pop();
+#else
+                data_bv.range(31, 0) = this->dma_read_chnl.read();
+#endif
+#if (__MNTR_CONNECTIONS__)
+                data_bv.range(63, 32) = this->dma_read_chnl.Pop();
+#else
+                data_bv.range(63, 32) = this->dma_read_chnl.read();
+#endif
+#endif
+                data = data_bv.to_uint64();
+
+                plm_local.data[i] = data;
+                if (ping) {
+                    ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): plm0_in.data[%d] := %llX", i, data);
+                } else {
+                    ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): plm1_in.data[%d] := %llX", i, data);
+                }
+            }
+            if (ping) {
+                plm0_in.write(plm_local);
+//              ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): plm0[%d] := %X", i, data);
+            } else {
+                plm1_in.write(plm_local);
+//              ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): plm1[%d] := %X", i, data);
+            }
+
+            this->load_compute_handshake();
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load() --> compute()");
+
+            ping = !ping;
+        }
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Load load(): batch # %d done!", n+1);
+    }
+
+    // Conclude
+    {
+        this->process_done();
+    }
+}
+
+
+void dummy::store_output()
+{
+
+    // Reset
+    {
+        HLS_PROTO("store-reset");
+        this->reset_store_output();
+        wait();
+    }
+
+    // Config
+    uint32_t tokens;
+    uint32_t batch;
+    {
+        HLS_PROTO("store-config");
+
+        wait_for_config(); // config process
+        conf_info_t config = this->conf_info.read();
+
+        tokens = config.tokens;
+        batch = config.batch;
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store config(): tokens = %llu, batch = %llu", ESP_TO_UINT64(tokens), ESP_TO_UINT64(batch));
+    }
+
+    // Store
+    bool ping = true;
+    uint32_t offset = 0;
+
+STORE_OUTPUT_BATCH_LOOP:
+    for (unsigned n = 0; n < batch; n++)
+    {
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store(): batch # %d start...", n+1);
+
+STORE_OUTPUT_TOKENS_LOOP:
+        for (int b = tokens; b > 0; b -= PLM_SIZE)
+        {
+            HLS_PROTO("store-dma");
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store(): ping = %d", ping);
+
+            this->store_compute_handshake();
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store() --> compute()");
+
+            uint32_t len = b > PLM_SIZE ? PLM_SIZE : b;
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store(): len = %llu [max %d]", ESP_TO_UINT64(len), PLM_SIZE);
+
+            dma_info_t dma_info(offset * DMA_BEAT_PER_WORD, len * DMA_BEAT_PER_WORD, DMA_SIZE);
+            offset += len;
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store(): dma_info.index = %llu, dma_info.length = %llu, dma_info.size = %llu", ESP_TO_UINT64(dma_info.index), ESP_TO_UINT64(dma_info.length), dma_info.size.to_uint64());
+
+#ifdef DMA_SINGLE_PROCESS
+#if (__MNTR_CONNECTIONS__)
+            this->dma_write_ctrl.PushNB(dma_info);
+#else
+            this->dma_write_ctrl.nb_write(dma_info);
+#endif
+#else
+#if (__MNTR_CONNECTIONS__)
+            this->dma_write_ctrl.Push(dma_info);
+#else
+            this->dma_write_ctrl.write(dma_info);
+#endif
+#endif
+            plm_t plm_local;
+            if (ping) {
+                plm_local = plm0_out.read();
+            } else {
+                plm_local = plm1_out.read();
+            }
+
+STORE_OUTPUT_DATA_LOOP:
+            for (uint16_t i = 0; i < len; i++) {
+
+                uint64_t data = plm_local.data[i];
+                if (ping) {
+                    ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store(): plm0_out.data[%d] -> %llX", i, data);
+                } else {
+                    ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store(): plm1_out.data[%d] -> %llX", i, data);
+                }
+                sc_dt::sc_bv<64> data_bv(data);
+
+#if (DMA_WIDTH == 64)
+
+#if (__MNTR_CONNECTIONS__)
+                this->dma_write_chnl.Push(data_bv);
+#else
+                this->dma_write_chnl.write(data_bv);
+#endif
+
+#elif (DMA_WIDTH == 32)
+
+#if (__MNTR_CONNECTIONS__)
+                this->dma_write_chnl.Push(data_bv.range(31, 0));
+                this->dma_write_chnl.Push(data_bv.range(64, 32));
+#else
+                this->dma_write_chnl.write(data_bv.range(31, 0));
+                this->dma_write_chnl.write(data_bv.range(64, 32));
+#endif
+
+#endif
+            }
+            ping = !ping;
+        }
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Store store(): batch # %d done!", n+1);
+    }
+
+    // Conclude
+    {
+        this->accelerator_done();
+        this->process_done();
+    }
+}
+
+
+void dummy::compute_kernel()
+{
+
+    // Reset
+    {
+        HLS_PROTO("compute-reset");
+        this->reset_compute_kernel();
+        wait();
+    }
+
+    // Config
+    uint32_t tokens;
+    uint32_t batch;
+    {
+        HLS_PROTO("compute-config");
+        wait_for_config(); // config process
+        conf_info_t config = this->conf_info.read();
+
+        tokens = config.tokens;
+        batch = config.batch;
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Compute config(): tokens = %llu, batch = %llu", ESP_TO_UINT64(tokens), ESP_TO_UINT64(batch));
+    }
+
+    // Compute
+    bool ping = true;
+
+COMPUTE_BATCH_LOOP:
+    for (unsigned n = 0; n < batch; n++)
+    {
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Compute compute(): batch # %d start...", n+1);
+
+COMPUTE_TOKENS_LOOP:
+        for (int b = tokens; b > 0; b -= PLM_SIZE)
+        {
+            this->compute_load_handshake();
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Compute compute() <---> load()");
+
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Compute compute(): ping = %d", ping);
+
+            uint32_t len = b > PLM_SIZE ? PLM_SIZE : b;
+
+            plm_t plm_local;
+            if (ping) {
+                plm_local = plm0_in.read();
+            } else {
+                plm_local = plm1_in.read();
+            }
+
+            for (unsigned i = 0; i < len; i++) {
+                plm_local.data[i]++;
+            }
+
+            if (ping) {
+                plm0_out.write(plm_local);
+            } else {
+                plm1_out.write(plm_local);
+            }
+
+ 
+            this->compute_store_handshake();
+            ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Compute compute() ---> store()");
+    
+            ping = !ping;
+        }
+
+        ESP_REPORT_TIME(VOFF, sc_time_stamp(), "Compute compute(): batch # %d done!", n+1);
+    }
+
+    // Conclude
+    {
+        this->process_done();
+    }
+}
+
+
+inline void dummy::reset_dma_read()
+{
+#if defined(__MNTR_CONNECTIONS__)
+    // Reset
+    dma_read_ctrl.Reset();
+    dma_read_chnl.Reset();
+#else
+    // Reset
+    dma_read_ctrl.reset_write();
+    dma_read_chnl.reset_read();
+#endif
+}
+
+inline void dummy::reset_dma_write()
+{
+#if defined(__MNTR_CONNECTIONS__)
+    // Reset
+    dma_write_ctrl.Reset();
+    dma_write_chnl.Reset();
+#else
+    // Reset
+    dma_write_ctrl.reset_write();
+    dma_write_chnl.reset_write();
+#endif
+}
+
+inline void dummy::reset_accelerator_done()
+{
+    acc_done.write(false);
+}
+
+// Utility functions
+
+//#pragma design modulario
+inline void dummy::process_done()
+{
+    //HLS_DEFINE_PROTOCOL("process-done");
+#pragma hls_unroll no
+PROCESS_DONE_LOOP:
+    do { wait(); } while (true);
+}
+
+//#pragma design modulario
+inline void dummy::accelerator_done()
+{
+    //HLS_DEFINE_PROTOCOL("accelerator-done");
+    acc_done.write(true); wait();
+    acc_done.write(false);
+}
+
+inline void dummy::reset_load_input()
+{
+#if defined(__MNTR_CONNECTIONS__)
+    input_ready.reset_req();
+#else
+    input_ready.req.reset_req();
+#endif
+    this->reset_dma_read();
+}
+
+inline void dummy::reset_compute_kernel()
+{
+#if defined(__MNTR_CONNECTIONS__)
+    input_ready.reset_ack();
+    output_ready.reset_req();
+#else
+    input_ready.ack.reset_ack();
+    output_ready.req.reset_req();
+#endif
+}
+
+inline void dummy::reset_store_output()
+{
+#if defined(__MNTR_CONNECTIONS__)
+    output_ready.reset_ack();
+#else
+    output_ready.ack.reset_ack();
+#endif
+    this->reset_accelerator_done();
+    this->reset_dma_write();
+}
+
+//#pragma design modulario
+inline void dummy::load_compute_handshake()
+{
+    {
+        //HLS_DEFINE_PROTOCOL("load-compute-handshake");
+#if defined(__MNTR_CONNECTIONS__)
+        input_ready.req();
+#else
+        input_ready.req.req();
+#endif
+    }
+}
+
+//#pragma design modulario
+inline void dummy::compute_load_handshake()
+{
+    {
+        //HLS_DEFINE_PROTOCOL("compute-load-handshake");
+#if defined(__MNTR_CONNECTIONS__)
+        input_ready.ack();
+#else
+        input_ready.ack.ack();
+#endif
+    }
+}
+
+//#pragma design modulario
+inline void dummy::compute_store_handshake()
+{
+    {
+        //HLS_DEFINE_PROTOCOL("compute-store-handshake");
+#if defined(__MNTR_CONNECTIONS__)
+        output_ready.req();
+#else
+        output_ready.req.req();
+#endif
+    }
+}
+
+//#pragma design modulario
+inline void dummy::store_compute_handshake()
+{
+    {
+        //HLS_DEFINE_PROTOCOL("store-compute-handshake");
+#if defined(__MNTR_CONNECTIONS__)
+        output_ready.ack();
+#else
+        output_ready.ack.ack();
+#endif
+    }
+}
+
+
+// Process
+//#pragma design modulario
+void dummy::config_accelerator()
+{
+    // HLS_DEFINE_PROTOCOL("config");
+    done.write(false); wait();
+    //ESP_REPORT_INFO("start configuration");
+    // Wait for the configuration signal
+    bool end = false;
+
+#pragma hls_unroll no
+CONFIG_LOOP:
+    do
+    {
+      wait();
+      end = conf_done.read();
+    } while (!end);
+
+    // Configuration completed
+    done.write(true);
+
+    //ESP_REPORT_INFO("end configuration");
+
+#pragma hls_unroll no
+CONFIG_DONE_LOOP:
+    while (true) { wait(); }
+}
+
+// Function
+
+inline void dummy::wait_for_config()
+{
+#pragma hls_unroll no
+WAIT_FOR_CONFIG_LOOP:
+    while (!done.read()) { wait(); }
+}
