1d0dabe411cf286bcd4a8faa661edc4552f77a6b
Paolo Mantovani <paolo@cs.columbia.edu>
2020-06-03 19:14:05 -0400

    Enable printf on RISC-V bare metal test programs

--- soft/leon3/drivers/synth/barec/synth.c
+++ soft/leon3/drivers/synth/barec/synth.c
@@ -9,9 +9,7 @@
  * Select Scatter-Gather in ESP configuration
  */
 
-#ifndef __riscv
 #include <stdio.h>
-#endif
 #include <stdlib.h>
 #include <esp_accelerator.h>
 #include <esp_probe.h>
@@ -42,11 +40,7 @@ int main(int argc, char * argv[])
 
 	ndev = probe(&espdevs, SLD_SYNTH, DEV_NAME);
 	if (!ndev) {
-#ifndef __riscv
 		printf("Error: %s device not found!\n", DEV_NAME);
-#else
-		print_uart("Error: "); print_uart(DEV_NAME); print_uart(" device not found!\n");
-#endif
 		exit(EXIT_FAILURE);
 	}
 
@@ -64,59 +58,35 @@ int main(int argc, char * argv[])
 			unsigned **ptable;
 			unsigned *mem;
 			int scatter_gather = 1;
-#ifndef __riscv
+
 			printf("******************** %s.%d ********************\n", DEV_NAME, n);
-#else
-			print_uart("******************** "); print_uart(DEV_NAME); print_uart(".");
-			print_uart_int(n); print_uart(" ********************\n");
-#endif
+
 			// Check if scatter-gather DMA is disabled
 			if (ioread32(dev, PT_NCHUNK_MAX_REG) == 0) {
-#ifndef __riscv
 				printf("  -> scatter-gather DMA is disabled; revert to contiguous buffer.\n");
-#else
-				print_uart("  -> scatter-gather DMA is disabled; revert to contiguous buffer.\n");
-#endif
 				scatter_gather = 0;
 			} else {
-#ifndef __riscv
 				printf("  -> scatter-gather DMA is enabled.\n");
-#else
-				print_uart("  -> scatter-gather DMA is enabled.\n");
-#endif
 			}
 
 			if (scatter_gather)
 				if (ioread32(dev, PT_NCHUNK_MAX_REG) < NCHUNK) {
-#ifndef __riscv
 					printf("  Trying to allocate %lu chunks on %d TLB available entries\n",
 						NCHUNK, ioread32(dev, PT_NCHUNK_MAX_REG));
-#else
-					print_uart("  Trying to allocate more chunks than available TLB entries\n");
-#endif
 					break;
 				}
 
 			// Allocate memory (will be contigous anyway in baremetal)
 			mem = aligned_malloc(SYNTH_BUF_SIZE);
-#ifndef __riscv
 			printf("  memory buffer base-address = %p\n", mem);
-#else
-			print_uart("  memory buffer base-address = 0x"); print_uart_int((unsigned long) mem); print_uart("\n");
-#endif
 
 			if (scatter_gather) {
 				//Alocate and populate page table
 				ptable = aligned_malloc(NCHUNK * sizeof(unsigned *));
 				for (i = 0; i < NCHUNK; i++)
 					ptable[i] = (unsigned *) &mem[i * (CHUNK_SIZE / sizeof(unsigned))];
-#ifndef __riscv
 				printf("  ptable = %p\n", ptable);
 				printf("  nchunk = %lu\n", NCHUNK);
-#else
-				print_uart("  ptable = 0x"); print_uart_int((unsigned long) ptable); print_uart("\n");
-				print_uart("  nchunk = 0x"); print_uart_int(NCHUNK); print_uart("\n");
-#endif
 			}
 
 			// Initialize input: write floating point hex values (simpler to debug)
@@ -142,11 +112,7 @@ int main(int argc, char * argv[])
 			esp_flush(coherence);
 
 			// Start accelerator
-#ifndef __riscv
 			printf("  Start..\n");
-#else
-			print_uart("  Start..\n");
-#endif
 			iowrite32(dev, CMD_REG, CMD_MASK_START);
 
 			done = 0;
@@ -155,11 +121,7 @@ int main(int argc, char * argv[])
 				done &= STATUS_MASK_DONE;
 			}
 			iowrite32(dev, CMD_REG, 0x0);
-#ifndef __riscv
 			printf("  Done\n");
-#else
-			print_uart("  Done\n");
-#endif
 
 			// Validation
 
@@ -167,11 +129,7 @@ int main(int argc, char * argv[])
 				aligned_free(ptable);
 			aligned_free(mem);
 
-#ifndef __riscv
 			printf("**************************************************\n\n");
-#else
-			print_uart("**************************************************\n\n");
-#endif
 		}
 	}
 	return 0;
